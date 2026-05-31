7d15db3a08c828d2848c656df7b0dd11d2d854fe
Nishith-S-Dev <saiyansnippet@gmail.com>
2026-06-01 02:52:53 +0530

    fix: add SSL for database connection on Vercel, add image domains

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "randomuser.me",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
     ],
   },
 };
